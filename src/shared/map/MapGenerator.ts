9020e52755ffa3759cf882e07cc32b6300bffb42
Algirdas Simutis <algirdas.simutis@hometogo.com>
2017-06-17 13:57:36 +0300

    Map rendering

--- src/shared/map/MapGenerator.ts
+++ src/shared/map/MapGenerator.ts
@@ -5,13 +5,36 @@ import {BLOCK_TYPE} from "../BlockType";
 export class MapGenerator {
     private map: Map;
 
-    generate(cols: number, rows: number) {
-        this.map = new Map(cols, rows);
+    generate(rows: number, cols: number) {
+        this.map = new Map(rows, cols);
 
-        this.map.setTile(new Point(1, 1), BLOCK_TYPE.SOLID);
-        this.map.setTile(new Point(3, 1), BLOCK_TYPE.SOLID);
-        this.map.setTile(new Point(5, 1), BLOCK_TYPE.SOLID);
-        this.map.setTile(new Point(7, 1), BLOCK_TYPE.SOLID);
+        for (let row = 0; row < cols; row++) {
+            this.map.setTile(new Point(row, 0), BLOCK_TYPE.SOLID);
+            this.map.setTile(new Point(row, rows-1), BLOCK_TYPE.SOLID);
+        }
+
+        for (let col = 0; col < rows; col++) {
+            this.map.setTile(new Point(0, col), BLOCK_TYPE.SOLID);
+            this.map.setTile(new Point(cols-1, col), BLOCK_TYPE.SOLID);
+        }
+
+
+        for (let row = 2; row < rows-2; row++) {
+            if (row % 2 != 0) {
+                continue;
+            }
+
+            for (let col = 2; col < cols - 2; col++) {
+                if (col % 2 == 0) {
+                    this.map.setTile(new Point(col, row), BLOCK_TYPE.SOLID);
+                }
+            }
+        }
+
+        // this.map.setTile(new Point(1, 1), BLOCK_TYPE.SOLID);
+        // this.map.setTile(new Point(3, 1), BLOCK_TYPE.SOLID);
+        // this.map.setTile(new Point(5, 1), BLOCK_TYPE.SOLID);
+        // this.map.setTile(new Point(7, 1), BLOCK_TYPE.SOLID);
     }
 
     public getMap() {
