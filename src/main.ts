f10a8f7751a1dc0bd4e0cd9110475d05540d19a6
MonikaKriksciuniene <monika.kriksciuniene@hometogo.com>
2017-06-17 10:25:06 +0300

    added import statements

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,8 @@
+import {Engine, InputManager, Renderer} from "./client/engine";
+
 const canvas:HTMLCanvasElement = <HTMLCanvasElement> document.getElementById('tutorial');
 const input = new InputManager(canvas);
 const renderer = new Renderer(canvas.getContext('2d'));
 const engine = new Engine(renderer, input);
+
 engine.start();
