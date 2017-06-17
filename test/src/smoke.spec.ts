c8024be4d211e79aa36d3ffbad5efe60051f6104
Arnas Juskevicius <juskeviciusarn@gmail.com>
2017-06-17 12:37:41 +0300

    Fills map

--- test/src/smoke.spec.ts
+++ test/src/smoke.spec.ts
@@ -1,7 +1,16 @@
 'use strict';
+import {GameState} from "../../src/server/GameState";
+
 
 describe('SmokeTest', function () {
     it('works', function () {
+        const gameState= new GameState();
+
+        const test = gameState.generateMap(11, 15);
+
         return 1 + 1 === 2
     })
 });
+
+
+// {action: “move”, start: {x: 1,y: 2}, direction: 1}
