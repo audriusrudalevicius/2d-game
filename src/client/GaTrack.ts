a4f674e25ca95411abf275370762750ab0f67b4c
d3trax <d3trax@gmail.com>
2017-06-20 22:51:10 +0300

    fix env

--- src/client/GaTrack.ts
+++ src/client/GaTrack.ts
@@ -1,18 +1,87 @@
-export class GaTrack {
-    private static ONCE_POINTS:TrackPoint[] = [];
+import * as Collections from "typescript-collections";
+import Queue from "typescript-collections/dist/lib/Queue";
 
-    public static TrackTimeOnce(trackPoint:TrackPoint) {
-        if (GaTrack.ONCE_POINTS.indexOf(trackPoint) !== -1) {
+export enum TrackingEventType {Timing};
+
+interface PendingTrackPoint {
+    type: TrackingEventType,
+    eventCategory: string,
+    eventAction: string,
+    eventValue: string|number
+}
+
+export class GaTrack {
+    private static instance: GaTrack;
+    private eventQueue: Queue<PendingTrackPoint> = new Collections.Queue<PendingTrackPoint>();
+    private onceHits: string[] = [];
+    private isGaLoaded:boolean = false;
+    private isSenderPending:boolean = false;
+
+    public static getInstance(): GaTrack {
+        if (!GaTrack.instance) {
+            GaTrack.instance = new GaTrack();
+        }
+        return GaTrack.instance;
+    }
+
+    public sendQueuedEvents()
+    {
+        if (typeof window === 'undefined' || typeof window.performance === 'undefined') {
+            this.eventQueue.forEach((qe) => {
+                console.log('Tracking Event removed', qe);
+            });
+            this.eventQueue.clear();
             return;
         }
-        if (typeof window !== 'undefined' && window.performance) {
-            ga('send', 'timing', trackPoint, 'load', Math.round(performance.now()));
+
+        if (!this.isGaLoaded) {
+            this.checkGA();
+            if (!this.isSenderPending) {
+                console.log('Waiting for trackers to able send queued events');
+                setTimeout(() => this.sendQueuedEvents(), 1000);
+                this.isSenderPending = true;
+            }
+            return;
         }
-        GaTrack.ONCE_POINTS.push(trackPoint);
+
+        while (!this.eventQueue.isEmpty()) {
+            const pendingEvent = this.eventQueue.dequeue();
+            console.log('Sending queued tracking event', pendingEvent);
+            switch (pendingEvent.type) {
+                case TrackingEventType.Timing:
+                    ga('send', 'timing', pendingEvent.eventCategory, pendingEvent.eventAction, pendingEvent.eventValue);
+                    break;
+            }
+        }
+        this.isSenderPending = false;
+    }
+
+    public scheduleTimedEvent(trackPoint: PendingTrackPoint, once:boolean = true) {
+        if (once && this.onceHits.indexOf(trackPoint.eventCategory) !== -1) {
+            return;
+        }
+        this.eventQueue.enqueue(trackPoint);
+        if (once) {
+            this.onceHits.push(trackPoint.eventCategory);
+        }
+        setTimeout(() => this.sendQueuedEvents(), 1);
+    }
+
+    public static TrackTimeOnce(trackPoint: string) {
+        GaTrack.getInstance().scheduleTimedEvent(<PendingTrackPoint>{
+            type: TrackingEventType.Timing,
+            eventCategory: trackPoint,
+            eventAction: 'load',
+            eventValue: Math.round(performance.now())
+        });
+    }
+
+    private checkGA() {
+        this.isGaLoaded = typeof ga !== 'undefined';
     }
 }
 
-export enum TrackPoint {
-    JS_LOAD = 'js_load',
-    GAME_CONNECT = 'game_connect'
+export class TrackPoint {
+    static JS_LOAD = 'js_load';
+    static GAME_CONNECT = 'game_connect';
 }
